adf70ac9a08c7aea1a43ed117b1874b46bf377c5
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-11-16 05:38:15 +0300

    added products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Products from './components/Products'
 
 const App = () => {
   return (
     <div>
    <Header /> 
-   <Hero /> 
+   <Hero />
+   <Products /> 
     </div>
   )
 }
