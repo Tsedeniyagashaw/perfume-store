593bba27b2b7fa34485f07e4db555d207781e300
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-11-28 01:54:37 +0300

    Add Footer

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -18,7 +18,7 @@ const categories = [
 
 const Categories = () => {
   return (
-    <section id='catagories' className='py-16 scroll-mt-20 bg-white'>
+    <section id='categories' className='py-16 scroll-mt-20 bg-white'>
      <div className='container mx-auto px-10'>
         <div className="text-center mb-12">
        <h2 className='text-3xl font-bold text-amber-950 mb-3'>
